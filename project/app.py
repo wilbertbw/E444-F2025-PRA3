65e942186fc0473a50a61285e239ae3637dd4a9f
wilbertbw <wilbertbwibowo@gmail.com>
2025-09-28 23:12:02 -0400

    added a schema for database; functions for database connection, creation, and closing; added test for database

--- project/app.py
+++ project/app.py
@@ -1,6 +1,33 @@
-from flask import Flask
+import sqlite3
+from flask import Flask, g
+
+DATABASE = "flaskr.db"
 
 app = Flask(__name__)
+
+app.config.from_object(__name__)
+
+def connect_db(): # connects to database
+  rv = sqlite3.connect(app.config["DATABASE"])
+  rv.row_factory = sqlite3.Row
+  return rv
+
+def init_db(): # create database
+  with app.app_context():
+    db = get_db()
+    with app.open_resource("schema.sql", mode="r") as f:
+      db.cursor().executescript(f.read())
+    db.commit()
+
+def get_db(): # open database connection
+  if not hasattr(g, "sqlite_db"):
+    g.sqlite_db = connect_db()
+  return g.sqlite_db
+
+@app.teardown_appcontext # close database connection
+def close_db(error):
+  if hasattr(g, "sqlite_db"):
+    g.sqlite_db.close()
 
 @app.route("/")
 def hello():
--- tests/app_test.py
+++ tests/app_test.py
@@ -1,4 +1,6 @@
-from project.app import app
+from pathlib import Path
+
+from project.app import app, init_db
 
 def test_index():
   tester = app.test_client()
@@ -6,3 +8,7 @@ def test_index():
 
   assert response.status_code == 200
   assert response.data == b"Hello, World!"
+
+def test_database():
+  init_db()
+  assert Path("flaskr.db").is_file()
