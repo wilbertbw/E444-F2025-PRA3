0ea188e2ecf18bc0c03f223c1517a5e9826e3210
wilbertbw <wilbertbwibowo@gmail.com>
2025-09-28 23:40:55 -0400

    added new routes (login, logout, add new post), updated base route

--- project/app.py
+++ project/app.py
@@ -1,7 +1,11 @@
 import sqlite3
-from flask import Flask, g
+from flask import Flask, g, render_template, request, session, flash, redirect, url_for, abort
 
+# configs
 DATABASE = "flaskr.db"
+USERNAME = "admin"
+PASSWORD = "admin"
+SECRET_KEY = "ece444_lab3"
 
 app = Flask(__name__)
 
@@ -30,8 +34,42 @@ def close_db(error):
     g.sqlite_db.close()
 
 @app.route("/")
-def hello():
-  return "Hello, World!"
+def index(): # searches database for entries and displays them
+  db = get_db()
+  cur = db.execute('select * from entries order by id desc')
+  entries = cur.fetchall()
+  return render_template('index.html', entries=entries)
+
+@app.route("/login", methods=['GET', 'POST'])
+def login(): # user authentication/session management
+  error = None
+  if request.method == 'POST':
+    if request.form['username'] != app.config['USERNAME']:
+      error = 'Invalid username'
+    elif request.form['password'] != app.config['PASSWORD']:
+      error = 'Invalid password'
+    else:
+      session['logged_in'] = True
+      flash('You were logged in')
+      return redirect(url_for('index'))
+  return render_template('login.html', error=error)
+
+@app.route("/logout")
+def logout(): # user logout/session management
+  session.pop('logged_in', None)
+  flash('You were logged out')
+  return redirect(url_for('index'))
+
+@app.route("/add",  methods=["POST"])
+def add_entry(): # add new post to database
+  if not session.get("logged_in"):
+    abort(401)
+  db = get_db()
+  db.execute('insert into entries (title, text) values (?, ?)', [request.form['title'], request.form['text']])
+  db.commit()
+  flash('New entry was successfully posted')
+  return redirect(url_for('index'))
+
 
 if __name__ == "__main__":
   app.run()
